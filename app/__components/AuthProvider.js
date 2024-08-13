318ec67488784d6b6ab789f5356e7d0523e10098
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-14 04:27:02 +0530

    new AuthProvider component is created that is using context api for user-data

---
+++ app/__components/AuthProvider.js
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useState } from "react";
+const user_context = React.createContext();
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("ecommerse-user"));
+    data && setUser(data);
+  }, []);
+
+  const login = (person) => {
+    setUser(person);
+    localStorage.setItem('classroom-management-user', JSON.stringify(person));
+  };
+
+  const logout = () => {
+    localStorage.setItem('classroom-management-user', JSON.stringify(null));
+    setUser(null);
+  };
+
+  const value = { user, login, logout };
+
+
+  return (
+    <user_context.Provider value={value}>
+      { children }
+    </user_context.Provider>
+  );
+}
+
+
+export const useAuth = () => {
+  return React.useContext(user_context);
+};
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "./__components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={``}>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
